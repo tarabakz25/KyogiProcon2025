ec676f490f708b915ddea7ca067ac5428a9ef4fe
Kz <aikikizuki@gmail.com>
2025-04-05 11:53:10 +0900

    環境構築

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "lib/json.hh"
+
+using namespace std;
+typedef long long ll;
+
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define REPD(i, n) for (int i = n - 1; i >= 0; i--)
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
